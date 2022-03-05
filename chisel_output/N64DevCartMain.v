bac65af60da096a36660a76abba239ffcf20baea
Edward Halferty <edhalferty@gmail.com>
2022-03-05 00:46:40 -0800

    stuff

--- chisel_output/N64DevCartMain.v
+++ chisel_output/N64DevCartMain.v
@@ -16,30 +16,30 @@ module N64DevCartMain(
   reg [31:0] _RAND_3;
   reg [31:0] _RAND_4;
 `endif // RANDOMIZE_REG_INIT
-  reg [15:0] memory [0:262143]; // @[N64DevCartMain.scala 13:21]
-  wire [15:0] memory__T_24_data; // @[N64DevCartMain.scala 13:21]
-  wire [17:0] memory__T_24_addr; // @[N64DevCartMain.scala 13:21]
-  reg [31:0] addr; // @[N64DevCartMain.scala 15:23]
-  wire  _T = ~io_n64aleh; // @[N64DevCartMain.scala 16:11]
-  reg  _T_2; // @[N64DevCartMain.scala 16:34]
-  wire  _T_3 = ~_T_2; // @[N64DevCartMain.scala 16:26]
-  wire  _T_4 = _T & _T_3; // @[N64DevCartMain.scala 16:23]
-  wire [31:0] _T_5 = addr & 32'hffff; // @[N64DevCartMain.scala 17:23]
-  wire [31:0] _T_7 = {io_n64adi, 16'h0}; // @[N64DevCartMain.scala 17:63]
-  wire [31:0] _T_8 = _T_5 | _T_7; // @[N64DevCartMain.scala 17:36]
-  wire  _T_9 = ~io_n64alel; // @[N64DevCartMain.scala 18:17]
-  reg  _T_11; // @[N64DevCartMain.scala 18:40]
-  wire  _T_12 = ~_T_11; // @[N64DevCartMain.scala 18:32]
-  wire  _T_13 = _T_9 & _T_12; // @[N64DevCartMain.scala 18:29]
-  wire [31:0] _T_14 = addr & 32'hffff0000; // @[N64DevCartMain.scala 19:23]
-  wire [31:0] _GEN_3 = {{16'd0}, io_n64adi}; // @[N64DevCartMain.scala 19:40]
-  wire [31:0] _T_16 = _T_14 | _GEN_3; // @[N64DevCartMain.scala 19:40]
-  reg  _T_18; // @[N64DevCartMain.scala 20:39]
-  wire  _T_19 = io_n64readn & _T_18; // @[N64DevCartMain.scala 20:29]
-  wire [31:0] _T_21 = addr + 32'h2; // @[N64DevCartMain.scala 21:22]
-  assign memory__T_24_addr = addr[18:1];
-  assign memory__T_24_data = memory[memory__T_24_addr]; // @[N64DevCartMain.scala 13:21]
-  assign io_n64ado = memory__T_24_data; // @[N64DevCartMain.scala 23:15]
+  reg [15:0] _T [0:262143]; // @[N64DevCartMain.scala 31:25]
+  wire [15:0] _T__T_26_data; // @[N64DevCartMain.scala 31:25]
+  wire [17:0] _T__T_26_addr; // @[N64DevCartMain.scala 31:25]
+  reg [31:0] _T_1; // @[N64DevCartMain.scala 33:27]
+  wire  _T_2 = ~io_n64aleh; // @[N64DevCartMain.scala 34:15]
+  reg  _T_4; // @[N64DevCartMain.scala 34:38]
+  wire  _T_5 = ~_T_4; // @[N64DevCartMain.scala 34:30]
+  wire  _T_6 = _T_2 & _T_5; // @[N64DevCartMain.scala 34:27]
+  wire [31:0] _T_7 = _T_1 & 32'hffff; // @[N64DevCartMain.scala 35:27]
+  wire [31:0] _T_9 = {io_n64adi, 16'h0}; // @[N64DevCartMain.scala 35:67]
+  wire [31:0] _T_10 = _T_7 | _T_9; // @[N64DevCartMain.scala 35:40]
+  wire  _T_11 = ~io_n64alel; // @[N64DevCartMain.scala 36:21]
+  reg  _T_13; // @[N64DevCartMain.scala 36:44]
+  wire  _T_14 = ~_T_13; // @[N64DevCartMain.scala 36:36]
+  wire  _T_15 = _T_11 & _T_14; // @[N64DevCartMain.scala 36:33]
+  wire [31:0] _T_16 = _T_1 & 32'hffff0000; // @[N64DevCartMain.scala 37:27]
+  wire [31:0] _GEN_3 = {{16'd0}, io_n64adi}; // @[N64DevCartMain.scala 37:44]
+  wire [31:0] _T_18 = _T_16 | _GEN_3; // @[N64DevCartMain.scala 37:44]
+  reg  _T_20; // @[N64DevCartMain.scala 38:43]
+  wire  _T_21 = io_n64readn & _T_20; // @[N64DevCartMain.scala 38:33]
+  wire [31:0] _T_23 = _T_1 + 32'h2; // @[N64DevCartMain.scala 39:26]
+  assign _T__T_26_addr = _T_1[18:1];
+  assign _T__T_26_data = _T[_T__T_26_addr]; // @[N64DevCartMain.scala 31:25]
+  assign io_n64ado = _T__T_26_data; // @[N64DevCartMain.scala 41:19]
 `ifdef RANDOMIZE_GARBAGE_ASSIGN
 `define RANDOMIZE
 `endif
@@ -77,17 +77,17 @@ initial begin
 `ifdef RANDOMIZE_MEM_INIT
   _RAND_0 = {1{`RANDOM}};
   for (initvar = 0; initvar < 262144; initvar = initvar+1)
-    memory[initvar] = _RAND_0[15:0];
+    _T[initvar] = _RAND_0[15:0];
 `endif // RANDOMIZE_MEM_INIT
 `ifdef RANDOMIZE_REG_INIT
   _RAND_1 = {1{`RANDOM}};
-  addr = _RAND_1[31:0];
+  _T_1 = _RAND_1[31:0];
   _RAND_2 = {1{`RANDOM}};
-  _T_2 = _RAND_2[0:0];
+  _T_4 = _RAND_2[0:0];
   _RAND_3 = {1{`RANDOM}};
-  _T_11 = _RAND_3[0:0];
+  _T_13 = _RAND_3[0:0];
   _RAND_4 = {1{`RANDOM}};
-  _T_18 = _RAND_4[0:0];
+  _T_20 = _RAND_4[0:0];
 `endif // RANDOMIZE_REG_INIT
   `endif // RANDOMIZE
 end // initial
@@ -97,16 +97,16 @@ end // initial
 `endif // SYNTHESIS
   always @(posedge clock) begin
     if (reset) begin
-      addr <= 32'h0;
-    end else if (_T_4) begin
-      addr <= _T_8;
-    end else if (_T_13) begin
-      addr <= _T_16;
-    end else if (_T_19) begin
-      addr <= _T_21;
+      _T_1 <= 32'h0;
+    end else if (_T_6) begin
+      _T_1 <= _T_10;
+    end else if (_T_15) begin
+      _T_1 <= _T_18;
+    end else if (_T_21) begin
+      _T_1 <= _T_23;
     end
-    _T_2 <= ~io_n64aleh;
-    _T_11 <= ~io_n64alel;
-    _T_18 <= ~io_n64readn;
+    _T_4 <= ~io_n64aleh;
+    _T_13 <= ~io_n64alel;
+    _T_20 <= ~io_n64readn;
   end
 endmodule
--- chisel_output/firrtl_black_box_resource_files.f
+++ chisel_output/firrtl_black_box_resource_files.f
@@ -1,1 +1,1 @@
-/Users/ed/n64/n64_dev_cart/chisel_output/N64DevCartMain.N64DevCartMain.memory.v
+/Users/ed/n64/n64_dev_cart/chisel_output/N64DevCartMain.N64DevCartMain._T.v
