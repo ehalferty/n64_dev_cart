eb61fb1e3719ac8f8f3a3232077e96bb2c5aa31d
Edward Halferty <edhalferty@gmail.com>
2022-03-11 05:36:09 -0800

    checkin

--- tmp/MisterSdram32MBController.v
+++ tmp/MisterSdram32MBController.v
@@ -2,33 +2,33 @@ module MisterSdram32MBController #(
   parameter NOP_SLOTS = 5,
   parameter REFRESH_AT = 300
 ) (
-  input             clock,
-  input             reset,
+  input             clk,
+  input             rst,
   input      [15:0] sdram_dq_in,
-  output reg [15:0] sdram_dq_out,
-  output reg [12:0] sdram_a,
-  output reg        sdram_we,
-  output reg        sdram_cas,
-  output reg        sdram_ras,
-  output reg        sdram_cs1,
-  output reg [1:0]  sdram_ba,
-  output reg        sdram_clk,
+  output reg [15:0] sdram_dq_out = 0,
+  output reg [11:0] sdram_a = 0,
+  output reg        sdram_we = 1,
+  output reg        sdram_cas = 1,
+  output reg        sdram_ras = 1,
+  output reg        sdram_cs1 = 0,
+  output reg [1:0]  sdram_ba = 0,
+  output reg        sdram_clk = 0,
   input             writeport_wr,
   input      [31:0] writeport_addr,
   input      [15:0] writeport_data,
-  output reg        writeport_ack,
+  output reg        writeport_ack = 0,
   input             readport_rd,
   input      [31:0] readport_addr,
-  output reg [15:0] readport_data,
-  input             readport_next,
-  output reg        readport_ack
+  output reg [15:0] readport_data = 0,
+//   input             readport_next,
+  output reg        readport_ack = 0
 );
 reg [3:0] refresh_state = 0;
 reg [3:0] write_state = 0;
 reg [3:0] read_state = 0;
 reg [31:0] refresh_counter = 0;
 reg [31:0] wait_counter = 0;
-always @(posedge clock) begin
+always @(posedge clk) begin
     if (refresh_state != 0) begin
     end else if (write_state != 0) begin
     end else if (read_state != 0) begin
@@ -36,7 +36,7 @@ always @(posedge clock) begin
         refresh_state <= 1;
         refresh_counter <= 0;
     end
-    sdramClk <= ~sdramClk;
+    sdram_clk <= ~sdram_clk;
 end
 endmodule
 
