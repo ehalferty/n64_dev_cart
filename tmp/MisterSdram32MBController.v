895cf8cd4c74452088d66670b3ce4247f87be7e9
Edward Halferty <edhalferty@gmail.com>
2022-03-11 19:19:49 -0800

    checkin

--- tmp/MisterSdram32MBController.v
+++ tmp/MisterSdram32MBController.v
@@ -22,6 +22,7 @@ module MisterSdram32MBController #(
 //   input             readport_next,
   output reg        readport_ack = 0
 );
+reg [3:0] setup_state = 1;
 reg [3:0] refresh_state = 0;
 reg [3:0] write_state = 0;
 reg [3:0] read_state = 0;
@@ -29,13 +30,13 @@ reg [31:0] refresh_counter = 0;
 reg [31:0] wt_cnt = 0;
 reg [15:0] latched_addr = 0;
 reg [31:0] latched_data = 0;
-reg initial_state = 1;
 reg [15:0] sdram_dq_reg = 0;
 reg writing = 0;
 assign sdram_dq = writing ? sdram_dq_reg : 16'bZ;
 
 `define INIT_WAIT wt_cnt <= 0;
-`define WAIT_NOP_SLOTS(done) `ISSUE_NOP; if (wt_cnt == NOP_SLOTS) begin; done; end else begin; wt_cnt <= wt_cnt + 1; end
+`define WAIT_NOP_SLOTS(done) `ISSUE_NOP; if (wt_cnt == NOP_SLOTS) begin; done; end \
+    else begin; wt_cnt <= wt_cnt + 1; end
 `define ISSUE_NOP sdram_we <= 1; sdram_cas <= 1; sdram_ras <= 1; writing <= 0;
 `define ISSUE_REFRESH sdram_we <= 1; sdram_cas <= 0; sdram_ras <= 0;
 `define ISSUE_BANK_ACTIVATE sdram_we <= 1; sdram_cas <= 1; sdram_ras <= 0; \
@@ -45,85 +46,118 @@ assign sdram_dq = writing ? sdram_dq_reg : 16'bZ;
 `define ISSUE_READ sdram_we <= 1; sdram_cas <= 0; sdram_ras <= 1; \
     sdram_ba <= latched_addr[24:23]; sdram_a <= 12'h400 + latched_addr[9:0];
 `define ISSUE_PRECHARGE_ALL sdram_we <= 0; sdram_cas <= 1; sdram_ras <= 0; sdram_a <= 12'h400;
+`define ISSUE_MODE_REGISTER_SET sdram_we <= 0; sdram_cas <= 0; sdram_ras <= 0; \
+    sdram_a <= 12'b001000110000; sdram_ba <= 2'b00;
 always @(posedge clk) begin
-    if (initial_state) begin
-        // TODO: Startup stuff
-        initial_state <= 0;
-    end else if (refresh_state != 0) begin
-        if (refresh_state == 1) begin
-            `ISSUE_REFRESH
-            refresh_state <= 2;
-            `INIT_WAIT
+    if (sdram_clk) begin
+        if (setup_state != 0) begin
+            if (setup_state == 1) begin
+                `ISSUE_NOP
+                setup_state <= 2;
+                `INIT_WAIT
+            end else if (setup_state == 2) begin
+                `WAIT_NOP_SLOTS(setup_state <= 3)
+            end else if (setup_state == 3) begin
+                `ISSUE_PRECHARGE_ALL
+                setup_state <= 4;
+                `INIT_WAIT
+            end else if (setup_state == 4) begin
+                `WAIT_NOP_SLOTS(setup_state <= 5)
+            end else if (setup_state == 5) begin
+                `ISSUE_REFRESH
+                setup_state <= 6;
+                `INIT_WAIT
+            end else if (setup_state == 6) begin
+                `WAIT_NOP_SLOTS(setup_state <= 7)
+            end else if (setup_state == 7) begin
+                `ISSUE_REFRESH
+                setup_state <= 8;
+                `INIT_WAIT
+            end else if (setup_state == 8) begin
+                `WAIT_NOP_SLOTS(setup_state <= 9)
+            end else if (setup_state == 9) begin
+                `ISSUE_MODE_REGISTER_SET
+                setup_state <= 10;
+                `INIT_WAIT
+            end else if (setup_state == 10) begin
+                `WAIT_NOP_SLOTS(setup_state <= 0)
+            end
+        end else if (refresh_state != 0) begin
+            if (refresh_state == 1) begin
+                `ISSUE_REFRESH
+                refresh_state <= 2;
+                `INIT_WAIT
+            end else begin
+                `WAIT_NOP_SLOTS(refresh_state <= 0)
+            end
+        end else if (write_state != 0) begin
+            if (write_state == 1) begin // Activate
+                `ISSUE_BANK_ACTIVATE
+                write_state <= 2;
+                `INIT_WAIT
+            end else if (write_state == 2) begin
+                `WAIT_NOP_SLOTS(write_state <= 3)
+            end else if (write_state == 3) begin
+                `ISSUE_WRITE
+                write_state <= 4;
+                `INIT_WAIT
+            end else if (write_state == 4) begin
+                `WAIT_NOP_SLOTS(write_state <= 5)
+            end else if (write_state == 5) begin
+                `ISSUE_PRECHARGE_ALL
+                write_state <= 6;
+                `INIT_WAIT
+            end else if (write_state == 6) begin
+                `WAIT_NOP_SLOTS(write_state <= 7)
+            end else if (write_state == 7) begin
+                if (!writeport_wr) begin
+                    writeport_ack <= 0;
+                    write_state <= 0;
+                end else begin
+                    writeport_ack <= 1;
+                end
+            end
+        end else if (read_state != 0) begin
+            if (read_state == 1) begin // Activate
+                `ISSUE_BANK_ACTIVATE
+                read_state <= 2;
+                `INIT_WAIT
+            end else if (read_state == 2) begin
+                `WAIT_NOP_SLOTS(read_state <= 3)
+            end else if (read_state == 3) begin
+                `ISSUE_READ
+                read_state <= 4;
+                `INIT_WAIT
+            end else if (read_state == 4) begin
+                `WAIT_NOP_SLOTS(read_state <= 5)
+                readport_data <= sdram_dq;
+            end else if (write_state == 5) begin
+                `ISSUE_PRECHARGE_ALL
+                read_state <= 6;
+                `INIT_WAIT
+            end else if (read_state == 6) begin
+                `WAIT_NOP_SLOTS(read_state <= 7)
+            end else if (read_state == 7) begin
+                if (!readport_rd) begin
+                    readport_ack <= 0;
+                    read_state <= 0;
+                end else begin
+                    readport_ack <= 1;
+                end
+            end
+        end else if (refresh_counter > REFRESH_AT) begin // QUESTION: Can a refresh squeeze in before a cart read in time???
+            refresh_state <= 1;
+            refresh_counter <= 0;
+        end else if (writeport_wr) begin
+            latched_addr <= writeport_addr;
+            latched_data <= writeport_data;
+            write_state <= 1;
+        end else if (readport_rd) begin
+            latched_addr <= writeport_addr;
+            read_state <= 1;
         end else begin
-            `WAIT_NOP_SLOTS(refresh_state <= 0)
+            refresh_counter <= refresh_counter + 1;
         end
-    end else if (write_state != 0) begin
-        if (write_state == 1) begin // Activate
-            `ISSUE_BANK_ACTIVATE
-            write_state <= 2;
-            `INIT_WAIT
-        end else if (write_state == 2) begin
-            `WAIT_NOP_SLOTS(write_state <= 3)
-        end else if (write_state == 3) begin
-            `ISSUE_WRITE
-            write_state <= 4;
-            `INIT_WAIT
-        end else if (write_state == 4) begin
-            `WAIT_NOP_SLOTS(write_state <= 5)
-        end else if (write_state == 5) begin
-            `ISSUE_PRECHARGE_ALL
-            write_state <= 6;
-            `INIT_WAIT
-        end else if (write_state == 6) begin
-            `WAIT_NOP_SLOTS(write_state <= 7)
-        end else if (write_state == 7) begin
-            if (!writeport_wr) begin
-                writeport_ack <= 0;
-                write_state <= 0;
-            end else begin
-                writeport_ack <= 1;
-            end
-        end
-    end else if (read_state != 0) begin
-        if (read_state == 1) begin // Activate
-            `ISSUE_BANK_ACTIVATE
-            read_state <= 2;
-            `INIT_WAIT
-        end else if (read_state == 2) begin
-            `WAIT_NOP_SLOTS(read_state <= 3)
-        end else if (read_state == 3) begin
-            `ISSUE_READ
-            read_state <= 4;
-            `INIT_WAIT
-        end else if (read_state == 4) begin
-            `WAIT_NOP_SLOTS(read_state <= 5)
-            readport_data <= sdram_dq;
-        end else if (write_state == 5) begin
-            `ISSUE_PRECHARGE_ALL
-            read_state <= 6;
-            `INIT_WAIT
-        end else if (read_state == 6) begin
-            `WAIT_NOP_SLOTS(read_state <= 7)
-        end else if (read_state == 7) begin
-            if (!readport_rd) begin
-                readport_ack <= 0;
-                read_state <= 0;
-            end else begin
-                readport_ack <= 1;
-            end
-        end
-    end else if (refresh_counter > REFRESH_AT) begin // QUESTION: Can a refresh squeeze in before a cart read in time???
-        refresh_state <= 1;
-        refresh_counter <= 0;
-    end else if (writeport_wr) begin
-        latched_addr <= writeport_addr;
-        latched_data <= writeport_data;
-        write_state <= 1;
-    end else if (readport_rd) begin
-        latched_addr <= writeport_addr;
-        read_state <= 1;
-    end else begin
-        refresh_counter <= refresh_counter + 1;
     end
     sdram_clk <= ~sdram_clk;
 end
